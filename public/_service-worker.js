c4f37085105456ea3bfe9e54608f192c617ee290
oldzodrac <113787256+oldzodrac@users.noreply.github.com>
2026-03-13 08:57:38 +0100

    Delete public/_service-worker.js

--- public/_service-worker.js
+++
@@ -1,7 +0,0 @@
-// Dummy service worker to satisfy Decap CMS
-self.addEventListener('install', () => {
-  self.skipWaiting();
-});
-self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
-});
